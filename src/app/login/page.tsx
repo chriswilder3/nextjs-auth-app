91b2741871f0a354ab64bc792f3258529c6cd396
chriswilder3 <chriswilderdelta19@gmail.com>
2025-02-11 23:40:56 +0530

    basic frontend part working correctly

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+    return (
+        <div className="bg-slate-700 flex flex-row justify-center font-serif">
+            <h1 className="text-2xl font-medium">
+               Welcome to Login Page!
+            </h1>
+        </div>
+    );
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function (){
+    return (
+        <div className="bg-indigo-700">
+            <h1 className="text-center text-white text-2xl font-medium">
+            Welcome to Sign up!
+            </h1>
+        </div>
+    )
+}
